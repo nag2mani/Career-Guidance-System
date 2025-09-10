501835ab695eaf8f1992c6162aa76402afc9cb3e
Nagmani kumar <115435469+nag2mani@users.noreply.github.com>
2025-09-10 12:40:57 +0530

    Update urls.py

--- career_guidance_backend/career_guidance_backend/urls.py
+++ career_guidance_backend/career_guidance_backend/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/',include('authentication.urls')),
